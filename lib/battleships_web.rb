ceec15946dd5319cd3d0aaec825b2fcaf4547c21
Edwardyao123 <zhigang_edward@hotmail.com>
2015-08-03 17:58:14 +0100

    fixed new game features, ask for player's name

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -2,9 +2,15 @@ require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
   get '/' do
-    'Hello BattleshipsWeb!'
+    erb :index
   end
+  set :views, proc {File.join(root, '..', 'views') }
 
+
+  get '/create' do
+  	@visitor = params[:name]
+  	erb :index1
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
